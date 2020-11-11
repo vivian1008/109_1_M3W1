9aa7e58b161f327e3f8df2527732c8079f43c8fb
vivian1008 <56061904+vivian1008@users.noreply.github.com>
2020-11-11 20:16:13 +0800

    Update M3Q2.java

--- M3Q2.java
+++ M3Q2.java
@@ -5,7 +5,8 @@ public class M3Q2{
         Scanner scn = new Scanner(System.in);
         Double v1 = scn.nextDouble();
         int v2 = scn.nextInt();
-        String str = "%."+v2+"f\n";
-        System.out.printf(str,v1);
+        //String str = "%."+v2+"f\n";
+        //System.out.print(str,v1);
+        System.out.println(Math.round(v1*Math.pow(10,v2))/Math.pow(10,v2));
     }
     }
